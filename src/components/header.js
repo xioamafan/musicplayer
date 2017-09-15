af38541c70819e50e35159a3f97113014140bc92
xioamafan <993144563+xiaomafan@users.noreply.github.com>
2017-09-15 20:17:30 +0800

    'tupianlujing'

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ let Header=React.createClass({
 	render(){
 		return(
 			<div className="components-header row">
-			   <img src="../../static/images/logo.png" width="40" className="-col-auto"/>
+			   <img src="../static/images/logo.png" width="40" className="-col-auto"/>
 			   <h1 className="caption">React Music Player</h1>
 			</div>
 		);
